f670b23f3f392cd1715d4b59a0322035050575ab
Ovy95 <jackoverton21@hotmail.com>
2021-01-18 21:25:17 +0000

    first commit left side complete

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Twitter</title>
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="frontend/assets/css/style.css">
+</head>
+<body>
+
+<section class="main-page">
+
+  <div class="left">
+    <div class="left-content">
+      <div>
+        <i class="fas fa-search"></i>
+        <h3 class="left-content-heading">Find Your Interests</h3>
+      </div>
+
+      <div>
+      <i class="fas fa-users"></i>
+        <h3 class="left-content-heading">Explore what people are talking about</h3>
+      </div>
+
+      <div>
+      <i class="fas fa-comments"></i>
+        <h3 class="left-content-heading"> Join the people</h3>
+      </div>
+
+    </div>
+  </div>
+
+
+
+
+
+
+    <div class="right">Right</div>
+      <footer>Footer</footer>
+   
+  
+</section>
+  
+</body>
+</html>
