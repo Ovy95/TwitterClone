e81ab3ff44bbef58863bff83308bd188d79b1e85
Ovy95 <jackoverton21@hotmail.com>
2021-01-19 22:07:20 +0000

    Styling complete for signup next is features

--- backend/shared/header.php
+++ backend/shared/header.php
@@ -13,6 +13,6 @@
   
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="frontend/assets/css/style.css">
+  <link rel="stylesheet" href="frontend/assets/css/master.css">
 </head>
 <body>
